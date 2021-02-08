a69a6cb51bf74fef5746bd89aa573801a98bf792
Shinya Nakamura <shinya913nano@icloud.com>
2021-02-08 23:51:14 +0900

    "change"

--- match/urls.py
+++ match/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('match_app.urls')),
     path('', include('user_app.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
